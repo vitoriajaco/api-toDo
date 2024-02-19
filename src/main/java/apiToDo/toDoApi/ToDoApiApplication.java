d630dee9a0960e64f19997a23207d441d3ebf148
vitoriajaco <vitoriajaco@gmail.com>
2024-02-19 14:46:58 -0300

    Commit inicial

---
+++ src/main/java/apiToDo/toDoApi/ToDoApiApplication.java
@@ -0,0 +1,13 @@
+package apiToDo.toDoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apiToDo/toDoApi/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package apiToDo.toDoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
