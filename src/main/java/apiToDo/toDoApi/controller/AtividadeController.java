31d9f5e6abeb7681cc98e0155a14d1428eed60bd
vitoriajaco <vitoriajaco@gmail.com>
2024-02-20 09:45:43 -0300

    Criando crud na controller

--- src/main/java/apiToDo/toDoApi/controller/AtividadeController.java
+++ src/main/java/apiToDo/toDoApi/controller/AtividadeController.java
@@ -1,7 +1,42 @@
 package apiToDo.toDoApi.controller;
 
+import apiToDo.toDoApi.model.AtividadeModel;
+import apiToDo.toDoApi.service.AtividadeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AtividadeController {
+
+    @Autowired
+    AtividadeService atividadeService;
+
+    @GetMapping(path = "/listarAtividades")
+    public List<AtividadeModel> mostrarTodasAtividades(){
+        return atividadeService.mostrarTodasAtividades();
+
+    }
+    @GetMapping(path = "/buscarPorId/{id}")
+    public Optional<AtividadeModel> buscarAtividadePorId(@PathVariable Long id){
+        return atividadeService.buscarAtividadePorId(id);
+
+    }
+
+    @PostMapping(path= "/cadastrarAtividade")
+    public AtividadeModel cadastrarAtividade (@RequestBody AtividadeModel atividadeModel){
+        return atividadeService.cadastrarAtividade(atividadeModel);
+    }
+
+    @PutMapping(path="/alterarAtividade/{id}")
+    public AtividadeModel alterarAtividade (@RequestBody AtividadeModel atividadeModel){
+        return atividadeService.alterarAtividade(atividadeModel);
+    }
+
+    public void deletarAtividade (@PathVariable Long id){
+        atividadeService.deletarAtividade(id);
+    }
 }
